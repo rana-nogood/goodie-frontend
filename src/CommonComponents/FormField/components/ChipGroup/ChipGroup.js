49f5061d33fb764533e1480a05660f15d8d55feb
rannasameh <rannasameh@hotmail.com>
2024-08-16 05:53:52 +0300

    remove unsed code

--- src/CommonComponents/FormField/components/ChipGroup/ChipGroup.js
+++ src/CommonComponents/FormField/components/ChipGroup/ChipGroup.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Typography, Button } from "@mui/material";
+import { Box, Button } from "@mui/material";
 
 const ChipGroup = ({ options, name, field, form }) => {
   const { value } = field;
